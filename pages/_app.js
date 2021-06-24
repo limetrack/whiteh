b5cadd8302157273d7fca8612361626e0298c361
Ivan Dovhal <ivan.dovhal@innovecs.com>
2021-06-23 20:03:27 -0400

    Jss styles preloader fix

--- pages/_app.js
+++ pages/_app.js
@@ -31,13 +31,13 @@ export default function MyApp({ Component, pageProps }) {
     router.events.on('routeChangeError', handleComplete);
   }, [router]);
 
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+  // useEffect(() => {
+  //   // Remove the server-side injected CSS.
+  //   const jssStyles = document.querySelector("#jss-server-side");
+  //   if (jssStyles) {
+  //     jssStyles.parentElement.removeChild(jssStyles);
+  //   }
+  // }, []);
 
   return (
     <>
