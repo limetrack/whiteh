38c87199f92cd95b6bc0869fe47719cb4d0127e5
Ivan Dovhal <ivan.dovhal@innovecs.com>
2021-07-05 05:48:16 -0400

    Safari ios shortcut fullscreen disable

--- pages/_document.js
+++ pages/_document.js
@@ -50,7 +50,7 @@ class MyDocument extends Document {
             name="keywords"
             content="whiteh,crypto,investments,bitcoin,cryptocoin,blockchain"
           />
-          <meta name="apple-mobile-web-app-capable" content="yes">
+          <meta name="apple-mobile-web-app-capable" content="no">
         </Head>
         <body>
           <Main />
